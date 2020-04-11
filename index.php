8961f94de4e7a7623dca1ca5594438a682dca414
azabraao <az.abraao@gmail.com>
2020-04-10 22:40:00 -0300

    Remove js desnecessario

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
   require_once('Usuario.php');
 
   session_start();
-  session_destroy();
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +10,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <title>Sistema de controle de usuário</title>
@@ -127,16 +125,5 @@
         </div>
     </section>
 </body>
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script>
-  $(document).ready(function () {
-      $('.datepicker').datepicker({
-          format: 'dd/mm/yyyy',                
-          language: 'pt-BR'
-      });
-  });
-</script>
 
 </html>
