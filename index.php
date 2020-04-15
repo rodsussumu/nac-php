22b5f1217a39b67643481106775d9fd204c181ee
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-04-14 22:19:36 -0300

    Criação do botão deletar usuario

--- index.php
+++ index.php
@@ -88,7 +88,19 @@
           </div>
 
           <button type="submit" name="submit" class="btn btn-outline-primary">Cadastrar</button>
-          <button type="reset"  class="btn btn-outline-danger">Limpar</button>
+          <button type="reset"  class="btn btn-outline-warning">Limpar</button>
+          <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseDeletar" aria-expanded="false" aria-controls="collapseExample">
+    Deletar Usuário
+  </button>
+  <p>
+  <div class="collapse" id="collapseDeletar">
+  <label for="clienteNome">Digite o nome do <strong>Usuário</strong> a ser deletado:</label>
+  <div class="row">
+    <input type="text" class="form-control col-8" placeholder="Digite o nome do usuário:">
+    <button type="button" class="btn btn-outline-danger ml-2" id="">Deletar</button>
+  </div>
+</div>
+</p>
           </form>
         </div>
     </section>
@@ -126,6 +138,10 @@
             </div>
         </div>
     </section>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
