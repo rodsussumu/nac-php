0fbcdac153ac0f8c29ede797933649662474fd7a
azabraao <az.abraao@gmail.com>
2020-04-17 17:31:59 -0300

    Remove comentário desnecessário

--- deleta-usuario.php
+++ deleta-usuario.php
@@ -4,10 +4,7 @@ session_start();
 
 if (isset($_GET['_indice']))
 {
-    // print_r(array_keys($_SESSION["usuarios"]));
-    // print_r($_SESSION["usuarios"]);
     unset($_SESSION["usuarios"][$_GET['_indice']]);
-
 }
 
 header('Location: index.php');
